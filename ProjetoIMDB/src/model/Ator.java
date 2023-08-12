73f12e8dd94870eb0cc85209c71a80b6fe13197f
Mateus Oliveira Milane <mateus.milane555@gmail.com>
2023-08-12 13:01:42 -0300

    Add Attributes and Methods

--- ProjetoIMDB/src/model/Ator.java
+++ ProjetoIMDB/src/model/Ator.java
@@ -1,4 +1,59 @@
 package model;
-
+import java.util.List;
 public class Ator {
+
+    private String papel;
+    private double tempoDeTela;
+    private String filmesPresente;
+    private String dataNascimento;
+    private String altura;
+    private boolean isOscar;
+
+    public String getPapel() {
+        return papel;
+    }
+
+    public void setPapel(String papel) {
+        this.papel = papel;
+    }
+
+    public double getTempoDeTela() {
+        return tempoDeTela;
+    }
+
+    public void setTempoDeTela(double tempoDeTela) {
+        this.tempoDeTela = tempoDeTela;
+    }
+
+    public String getFilmesPresente() {
+        return filmesPresente;
+    }
+
+    public void setFilmesPresente(String filmesPresente) {
+        this.filmesPresente = filmesPresente;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getAltura() {
+        return altura;
+    }
+
+    public void setAltura(String altura) {
+        this.altura = altura;
+    }
+
+    public boolean isOscar() {
+        return isOscar;
+    }
+
+    public void setOscar(boolean oscar) {
+        isOscar = oscar;
+    }
 }
--- ProjetoIMDB/src/model/Diretor.java
+++ ProjetoIMDB/src/model/Diretor.java
@@ -1,4 +1,14 @@
 package model;
 
 public class Diretor {
+
+    private int numFilmesDirigidos;
+
+    public int getNumFilmesDirigidos() {
+        return numFilmesDirigidos;
+    }
+
+    public void setNumFilmesDirigidos(int numFilmesDirigidos) {
+        this.numFilmesDirigidos = numFilmesDirigidos;
+    }
 }

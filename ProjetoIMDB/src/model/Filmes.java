3ac705f60e39a7cf3f950eee1afc23548f8d62f0
IurydJR <iury.djr.ti@gmail.com>
2023-08-13 13:10:34 -0300

    feat(Filmes): Adiciona atributos base na classe Filmes

--- ProjetoIMDB/src/model/Filmes.java
+++ ProjetoIMDB/src/model/Filmes.java
@@ -1,4 +1,22 @@
 package model;
 
+import enums.CategoriaFilmes;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public class Filmes {
+    private String nome;
+    private String dataLancamento;
+    private BigDecimal orcamento;
+    private String descricao;
+    private byte classificacaoIndicativa;
+
+    private List<Ator> ator;
+    private List<Diretor> diretor;
+
+    private CategoriaFilmes categoriaFilme;
+
+
+
 }

7f9afc600eb82a0d5fbf5ee8dc6c7469313cff28
IurydJR <iury.djr.ti@gmail.com>
2023-08-13 19:02:29 -0300

    feat(Filmes): Adiciona métodos set/get para os atributos

--- ProjetoIMDB/src/model/Filmes.java
+++ ProjetoIMDB/src/model/Filmes.java
@@ -11,13 +11,59 @@ public class Filmes {
     private String dataLancamento;
     private BigDecimal orcamento;
     private String descricao;
-    private ClassificacaoIndicativa classificacaoIndicativa;
+
 
     private List<Ator> ator;
     private List<Diretor> diretor;
 
     private CategoriaFilmes categoriaFilme;
+    private ClassificacaoIndicativa classificacaoIndicativa;
 
+    public String getNome() {
+        return nome;
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
+    public String getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(String dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public BigDecimal getOrcamento() {
+        return orcamento;
+    }
+
+    public void setOrcamento(BigDecimal orcamento) {
+        this.orcamento = orcamento;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public CategoriaFilmes getCategoriaFilme() {
+        return categoriaFilme;
+    }
+
+    public void setCategoriaFilme(CategoriaFilmes categoriaFilme) {
+        this.categoriaFilme = categoriaFilme;
+    }
+
+    public ClassificacaoIndicativa getClassificacaoIndicativa() {
+        return classificacaoIndicativa;
+    }
+
+    public void setClassificacaoIndicativa(ClassificacaoIndicativa classificacaoIndicativa) {
+        this.classificacaoIndicativa = classificacaoIndicativa;
+    }
 }

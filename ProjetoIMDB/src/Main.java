47b9a081a3f1413ffd2786488f125a953fea5a16
JayPih <jp_knot@hotmail.com>
2023-08-14 17:16:25 -0300

    Criando a classe main e algumas funções.

--- ProjetoIMDB/src/Main.java
+++ ProjetoIMDB/src/Main.java
@@ -1,11 +1,71 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import model.Filmes;
+import model.Ator;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello and welcome!");
+        List<Filmes> catalogo = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
+        while (true) {
+            System.out.println("Selecione uma opção:");
+            System.out.println("1 - Cadastrar Filme");
+            System.out.println("2 - Cadastrar Ator");
+            System.out.println("3 - Cadastrar Diretor");
+            System.out.println("4 - Associar Filme com Ator/Diretor");
+            System.out.println("5 - Pesquisar Filme por Nome");
+            System.out.println("6 - Sair");
 
-            System.out.println("i = " + i);
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    // Lógica para cadastrar filme
+                    break;
+                case 2:
+                    // Lógica para cadastrar ator
+                    break;
+                case 3:
+                    // Lógica para cadastrar diretor
+                    break;
+                case 4:
+                    // Lógica para associar ator/diretor a um filme
+                    break;
+                case 5:
+                    System.out.println("Digite o nome do filme para pesquisa:");
+                    String nomePesquisa = scanner.nextLine();
+                    pesquisarFilmePorNome(catalogo, nomePesquisa);
+                    break;
+                case 6:
+                    System.out.println("Encerrando o programa...");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
         }
+    }
+
+    public static void pesquisarFilmePorNome(List<Filmes> catalogo, String nomePesquisa) {
+        for (Filmes filme : catalogo) {
+            if (filme.getNome().equalsIgnoreCase(nomePesquisa)) {
+                System.out.println("Filme Encontrado:");
+                System.out.println("Nome: " + filme.getNome());
+                System.out.println("Data de Lançamento: " + filme.getDataLancamento());
+                System.out.println("Orçamento: " + filme.getOrcamento());
+                System.out.println("Descrição: " + filme.getDescricao());
+                System.out.println("Diretor: " + filme.getDiretor().getNome());
+                System.out.println("Atores: ");
+                for (Ator ator : filme.getAtores()) {
+                    System.out.println("- " + ator.getNome());
+                }
+                return;  // Sai da função depois de encontrar o filme
+            }
+        }
+        System.out.println("Nenhum filme encontrado com o nome informado.");
     }
 }
--- ProjetoIMDB/src/model/Filmes.java
+++ ProjetoIMDB/src/model/Filmes.java
@@ -1,4 +1,82 @@
 package model;
 
+import enums.CategoriaFilmes;
+import enums.ClassificacaoIndicativa;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public class Filmes {
+    private String nome;
+    private String dataLancamento;
+    private BigDecimal orcamento;
+    private String descricao;
+    private boolean isOscar;
+
+    private List<Ator> ator;
+    private List<Diretor> diretor;
+
+    private CategoriaFilmes categoriaFilme;
+    private ClassificacaoIndicativa classificacaoIndicativa;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(String dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public BigDecimal getOrcamento() {
+        return orcamento;
+    }
+
+    public void setOrcamento(BigDecimal orcamento) {
+        this.orcamento = orcamento;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public CategoriaFilmes getCategoriaFilme() {
+        return categoriaFilme;
+    }
+
+    public void setCategoriaFilme(CategoriaFilmes categoriaFilme) {
+        this.categoriaFilme = categoriaFilme;
+    }
+
+    public ClassificacaoIndicativa getClassificacaoIndicativa() {
+        return classificacaoIndicativa;
+    }
+
+    public void setClassificacaoIndicativa(ClassificacaoIndicativa classificacaoIndicativa) {
+        this.classificacaoIndicativa = classificacaoIndicativa;
+    }
+    public boolean isOscar() {
+        return isOscar;
+    }
+
+    public void setOscar(boolean oscar) {
+        isOscar = oscar;
+    }
+
+    public String infoFilme(){
+        return this.nome +"("+ this.dataLancamento +")"+"["+ this.classificacaoIndicativa +"]";
+    }
+
 }

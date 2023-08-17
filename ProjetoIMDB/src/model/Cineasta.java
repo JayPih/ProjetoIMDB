2f52515e9b82a0a92109a37d6926e8b5580c0423
Mateus Oliveira Milane <mateus.milane555@gmail.com>
2023-08-17 19:44:39 -0300

    Create extends class Cineasta

--- ProjetoIMDB/src/model/Cineasta.java
+++ ProjetoIMDB/src/model/Cineasta.java
@@ -1,4 +1,53 @@
 package model;
 
-public class Cineasta {
+import java.util.Date;
+
+public abstract class Cineasta {
+    private String nome;
+    private String sobrenome;
+    private Date dataNascimento;
+    private double altura;
+    private boolean isOscar;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public boolean isOscar() {
+        return isOscar;
+    }
+
+    public void setOscar(boolean oscar) {
+        isOscar = oscar;
+    }
+
+
 }
